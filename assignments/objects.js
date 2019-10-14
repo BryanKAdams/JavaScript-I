4b97c502cd385660dd0b2ff52b4473fa45073042
BryanKAdams <17adamsbryk@washk12.org>
2019-10-14 14:55:58 -0600

    finished arrays, finishing up on objects

--- assignments/objects.js
+++ assignments/objects.js
@@ -19,34 +19,47 @@ const example = {
 
 // Write your intern objects here:
 const database = [
-  {id: 1, email: "mmelloy0@psu.edu" firstName: "Mitzi" Gender: "F"}
-  {id: 2, email: "kdiben1@tinypic.com" firstName: "Kennan" Gender: "M"}
-  {id: 3, email: "kmummery2@wikimedia.org" firstName: "Keven" Gender: "M"}
-  {id: 4, email: "gmartinson3@illinois.edu" firstName: "Gannie" Gender: "M"}
-  {id: 5, email: "adaine5@samsung.com" firstName: "Antonietta" Gender: "F"}
-]
+  { id: 1, name: "Mitzi", email: "mmelloy0@psu.edu", gender: "F" },
+  { id: 2, name: "Kennan", email: "kdiben1@tinypic.com", gender: "M" },
+  { id: 3, name: "Keven", email: "kmummery2@wikimedia.org", gender: "M" },
+  { id: 4, name: "Gannie", email: "gmartinson3@illinois.edu", gender: "M" },
+  { id: 5, name: "Antonietta", email: "adaine5@samsung.com", gender: "F" }
+];
+console.log(database);
 
 
 // ==== Challenge 2: Reading Object Data ==== 
 // Once your objects are created, log out the following requests from HR into the console:
 
 // Mitzi's name
+console.log(database[0].name);
 
 // Kennan's ID
+console.log(database[1].id);
 
 // Keven's email
+console.log(database[2].email);
 
 // Gannie's name
+console.log(database[3].name);
 
 // Antonietta's Gender
+console.log(database[4].gender);
 
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 // console.log(kennan.speak());
-
+const speak = function(){
+  return 'Hello, my name is ' + this.name + '!';
+ }
+ database[1].speak = speak;
+ console.log(database[1].speak());
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
-//console.log(antonietta.multiplyNums(3,4));
-
+const mult = function(a, b){
+  return a * b;
+}
+database[4].multiplyNums = mult;
+console.log(database[4].multiplyNums(3,4));
 // === Great work! === Head over to the the arrays.js. You may come back and attempt the Stretch Challenge once you have completed the challenges in arrays.js and function-conversion.js.
 
 // ==== Stretch Challenge: Nested Objects and the this keyword ==== 
